3d98637bec730e11c2aeecfe38821f95d7f2bea8
Jordan Israel Diaz <diazjordan@cicese.edu.mx>
2021-07-17 21:58:04 -0700

    Parametric curve for helicoid was added

--- examples/vector-curve-function.py
+++ examples/vector-curve-function.py
@@ -1,6 +1,10 @@
+from math import sin, cos, PI
+import matplotlib.pyplot as plt
 from odeanimate.vector import Vector
 
+@Vector.codomain
+def helicoid(t):
+    return 3*cos(2*PI*t), 2*sin(2*PI*t), 3*t/4
+
 if __name__ == '__main__':
-    a = Vector(1, 2, 3)
-    b = Vector(3, 2, 1)
-    print(a + b)
+    pass

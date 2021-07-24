b1ab29253db272215ffa74082c2f64b8d946ffc5
Miguel Alejandro Salgado Zapien <salgado.miguel@uabc.edu.mx>
2021-07-24 10:50:49 -0700

    Added normalization to gramschmitd method and output method

--- examples/vector-gram-schmidt.py
+++ examples/vector-gram-schmidt.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
+from examples.output import output_file
 from odeanimate.vector import Vector
 
 
 def Gram(*non):
     orth = []
     for (i, cur) in enumerate(non, start=0):
-        x = cur - sum([
+        x = (cur - sum([
             ((cur @ other) / abs(other)**2)*other
             for other in orth[:i]
-        ], start=Vector(0, 0, 0))
-        orth.append(x)
+        ], start=Vector(0, 0, 0)))
+        orth.append(x / abs(x))
     return orth
 
 
@@ -18,20 +19,21 @@ if __name__ == '__main__':
     ortho_base = Gram(*non_ortho_base)
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(projection='3d')
-    ax.set_xlim(0, 4)
-    ax.set_ylim(0, 4)
-    ax.set_zlim(0, 4)
+    ax.set_xlim(-3, 3)
+    ax.set_ylim(-3, 3)
+    ax.set_zlim(-3, 3)
+    ax.scatter([0], [0], [0], color='blue', label='Origin')
     for (orig, ortho) in zip(non_ortho_base, ortho_base):
         ax.quiver(
             [0], [0], [0],
             [orig.values[0]], [orig.values[1]], [orig.values[2]],
-            color='k',
-            label='Original Base'
+            color='green',
         )
         ax.quiver(
             [0], [0], [0],
             [ortho.values[0]], [ortho.values[1]], [ortho.values[2]],
-            color='r',
-            label='Orthonormal Base'
+            color='red',
         )
-    ax.view_init(10, 40)
+    ax.view_init(10, -160)
+    image_file = output_file(__file__, '.png')
+    fig.savefig(image_file)

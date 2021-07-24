fd05f2e7531114df4686a43e96346cc257ade64d
Miguel Alejandro Salgado Zapien <salgado.miguel@uabc.edu.mx>
2021-07-24 10:51:37 -0700

    Rename gramschmidt function

--- examples/vector-gram-schmidt.py
+++ examples/vector-gram-schmidt.py
@@ -3,7 +3,7 @@ from examples.output import output_file
 from odeanimate.vector import Vector
 
 
-def Gram(*non):
+def GramSchmidtOrthoNormalize(*non):
     orth = []
     for (i, cur) in enumerate(non, start=0):
         x = (cur - sum([
@@ -16,7 +16,7 @@ def Gram(*non):
 
 if __name__ == '__main__':
     non_ortho_base = [Vector(1, -1, 1), Vector(1, 0, 1), Vector(1, 1, 2)]
-    ortho_base = Gram(*non_ortho_base)
+    ortho_base = GramSchmidtOrthoNormalize(*non_ortho_base)
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(projection='3d')
     ax.set_xlim(-3, 3)
